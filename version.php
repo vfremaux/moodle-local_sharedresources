e78a29e27d7dce5442fee1b1794a4c42be9c6a6e
valery <valery.fremaux@gmail.com>
2016-04-12 10:21:59 +0200

    adjust version

--- version.php
+++ version.php
@@ -17,8 +17,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2014032700;   // The (date) version of this plugin
-$plugin->requires = 2011120500;   // Requires this Moodle version
+$plugin->requires = 2014110400;   // Requires this Moodle version
 $plugin->component = 'local_sharedresources'; 
-$plugin->release = '2.7.0 (Build 2014032700)';
-$plugin->maturity = MATURITY_BETA;
+$plugin->release = '2.8.0 (Build 2014032700)';
+$plugin->maturity = MATURITY_RC;
 $plugin->dependencies = array('mod_sharedresource' => 2013032600);
