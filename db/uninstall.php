b9a144aa6fb3523feb7654d3c732a9dfd9da587b
Valery Fremaux <valery.fremaux@gmail.com>
2024-04-27 09:44:50 +0200

    Automated sync from SVN

---
+++ db/uninstall.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Post-uninstall code for the sharedresource local plugin.
+ *
+ * @package     local_sharedresource
+ * @category    local
+ * @copyright   2013 Valery Fremaux
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * on the install we still need to build and add the librarian role.
+ */
+function xmldb_local_sharedresources_uninstall() {
+    global $DB;
+
+    $result = true;
+
+    // Remove the teacherowner role if absent.
+    if ($oldrole = $DB->get_record('role', array('shortname' => 'librarian'))) {
+        delete_role($oldrole->id);
+    }
+
+    return $result;
+}
